3a2b4d99d94f2cae9528ee45ff391609de92fb11
EminentZ <1120545752@qq.com>
2019-07-10 10:11:07 +0800

    添加user

--- core/src/main/java/cn/hwsoft/wisdom/core/domain/User.java
+++ core/src/main/java/cn/hwsoft/wisdom/core/domain/User.java
@@ -4,12 +4,30 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Setter@Getter@ToString
+@Setter
+@Getter
+@ToString
 public class User {
     private Integer id;
 
-    private String name;
+    private String identity;
 
-    private String age;
+    private String nickname;
+
+    private String realname;
+
+    private String password;
+
+    private Integer phone;
+
+    private String cover;
+
+    private String address;
+
+    private Byte status;
+
+    private Integer create_time;
+
+    private Integer update_time;
 
 }
